a8633b249e78df3b41fbd516fa6d3497d03a6d16
Amir-HShahi <amir13831107h@gmail.com>
2024-08-24 20:37:10 +0330

    utility.dart: implemented to scale length according to design

---
+++ lib/view/components/app_bar_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends AppBar {
+  AppBarWidget({super.key});
+
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
--- lib/view/qr_codes_screen.dart
+++ lib/view/qr_codes_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:url_shortener/utility.dart';
 
 class QrCodesScreen extends StatelessWidget {
   const QrCodesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // appBar: AppBar(
+      //   leading: Image(
+      //     image: const AssetImage("assets/images/app_bar_icon.png"),
+      //     height: scaledHeight(2),
+      //     width: scaledWidth(1),
+      //   ),
+      // ),
+      body:  SafeArea(child: Image(
+          image: const AssetImage("assets/images/app_bar_icon.png"),
+          height: scaledHeight(100),
+          width: scaledWidth(50),
+        )),
+    );
   }
 }

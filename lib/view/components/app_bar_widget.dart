e1688c2de32f89e9798164b8fee9b3940e1d8fea
Amir-HShahi <amir13831107h@gmail.com>
2024-08-24 22:25:10 +0330

    Add: app_bar_widget.dart

--- lib/view/components/app_bar_widget.dart
+++ lib/view/components/app_bar_widget.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:url_shortener/utility.dart';
 
-class AppBarWidget extends AppBar {
-  AppBarWidget({super.key});
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({super.key});
+  void pressHomeButtonHandler() {
+    //push to home screen
+  }
 
+  @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Container(
+      height: scaledHeight(64),
+      width: double.infinity,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(width: scaledWidth(16)),
+          Image(
+              image: const AssetImage("assets/images/app_bar_icon.png"),
+              height: scaledHeight(32)),
+          SizedBox(width: scaledWidth(8)),
+          Image(
+              image: const AssetImage("assets/images/app_bar_typo_icon.png"),
+              height: scaledHeight(22)),
+          const Spacer(),
+          GestureDetector(
+            onTap: pressHomeButtonHandler,
+            child: Image(
+                image: const AssetImage("assets/images/home_screen_button.png"),
+                height: scaledHeight(16)),
+          ),
+          SizedBox(width: scaledWidth(16))
+        ],
+      ),
+    );
   }
 }
--- lib/view/qr_codes_screen.dart
+++ lib/view/qr_codes_screen.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:url_shortener/utility.dart';
+import 'package:url_shortener/view/components/app_bar_widget.dart';
 
 class QrCodesScreen extends StatelessWidget {
   const QrCodesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   leading: Image(
-      //     image: const AssetImage("assets/images/app_bar_icon.png"),
-      //     height: scaledHeight(2),
-      //     width: scaledWidth(1),
-      //   ),
-      // ),
-      body:  SafeArea(child: Image(
-          image: const AssetImage("assets/images/app_bar_icon.png"),
-          height: scaledHeight(100),
-          width: scaledWidth(50),
-        )),
+    return const Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [AppBarWidget()],
+      )),
     );
   }
 }

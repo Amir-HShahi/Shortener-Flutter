ff351e13512a825089a23ec45d99ef14e37f01f5
Amir-HShahi <amir13831107h@gmail.com>
2024-09-04 23:46:03 +0330

    QrCodeDetailsScreen implemented

--- lib/view/components/save_qr_code_button.dart
+++ lib/view/components/save_qr_code_button.dart
@@ -1,14 +1,34 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:url_shortener/utility.dart';
 
 class SaveQrCodeButton extends StatelessWidget {
   const SaveQrCodeButton({super.key});
+  void saveToGalleryHandler() {}
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: scaledHeight(32),
-      width: scaledWidth(200),
+    return GestureDetector(
+      onTap: saveToGalleryHandler,
+      child: Container(
+        height: scaledHeight(40),
+        width: scaledWidth(345),
+        decoration: BoxDecoration(
+          color: const Color(0xff1B48DA),
+          borderRadius: BorderRadius.all(Radius.circular(scaledHeight(6))),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.add, size: scaledHeight(20), color: Colors.white),
+            SizedBox(width: scaledWidth(4)),
+            Text("Add to gallery",
+                style: GoogleFonts.manrope(
+                    fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white))
+          ],
+        ),
+      ),
     );
   }
 }

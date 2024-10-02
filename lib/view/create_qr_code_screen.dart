4e9ac63acb7bf73c7eb2c4fb7c7eceafaf52c253
Amir-HShahi <amir13831107h@gmail.com>
2024-10-02 10:14:13 +0330

    Crate qrCode logic implemented

--- lib/view/create_qr_code_screen.dart
+++ lib/view/create_qr_code_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:url_shortener/view/components/customized_floating_button.dart';
 import 'package:url_shortener/view/components/customized_text_field.dart';
+import 'package:url_shortener/view_model/qr_code_view_model.dart';
 
 import '../utility.dart';
 import 'components/app_bar_widget.dart';
@@ -10,10 +12,23 @@ class CreateQrCodeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    void saveQrCodeHandler() {}
-
     TextEditingController companyName = TextEditingController();
     TextEditingController companyAddress = TextEditingController();
+
+    void saveQrCodeHandler() {
+      if (companyName.text.isNotEmpty && companyAddress.text.isNotEmpty) {
+        var viewModel = context.read<QrCodeViewModel>();
+        viewModel.addQrCode(companyName.text, companyAddress.text);
+        Navigator.pop(context);
+      } else {
+        var textTheme = Theme.of(context).textTheme;
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "Name or address can't be empty",
+          style: textTheme.displayMedium,
+        )));
+      }
+    }
 
     return Scaffold(
       appBar: AppBarWidget(isHomeButtonActive: true),

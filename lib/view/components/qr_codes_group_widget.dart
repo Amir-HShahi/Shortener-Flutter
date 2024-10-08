94ad63572b0947e7404e1c0186589489d13cf5d8
Amir-HShahi <amir13831107h@gmail.com>
2024-10-08 15:55:51 +0330

    Qr codes group become scorable

--- lib/view/components/qr_codes_group_widget.dart
+++ lib/view/components/qr_codes_group_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:url_shortener/model/qr_code_model.dart';
 import 'package:url_shortener/utility.dart';
 import 'package:url_shortener/view/components/qr_code_tile_widget.dart';
 import 'package:url_shortener/view_model/qr_code_view_model.dart';
@@ -12,41 +11,48 @@ class QrCodesGroupWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var viewModel = context.watch<QrCodeViewModel>();
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.symmetric(vertical: scaledHeight(16)),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(12)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 1,
-              blurRadius: 3,
-              offset: const Offset(0, 2), // changes position of shadow
-            ),
-          ]),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: scaledWidth(16)),
-            child: Text(
-              "QR Codes",
-              style: GoogleFonts.manrope(
-                  fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: scaledHeight(650)
+      ),
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: scaledHeight(16)),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 3,
+                offset: const Offset(0, 2), // changes position of shadow
+              ),
+            ]),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: scaledWidth(16)),
+                child: Text(
+                  "QR Codes",
+                  style: GoogleFonts.manrope(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: scaledHeight(16)),
+              for (var model in viewModel.qrCodes)
+                Column(
+                  children: [
+                    const _DividerLineWidget(),
+                    QrCodeTileWidget(qrCodeModel: model),
+                  ],
+                ),
+            ],
           ),
-          SizedBox(height: scaledHeight(16)),
-          for (var model in viewModel.qrCodes)
-            Column(
-              children: [
-                const _DividerLineWidget(),
-                QrCodeTileWidget(qrCodeModel: model),
-              ],
-            ),
-        ],
+        ),
       ),
     );
   }
